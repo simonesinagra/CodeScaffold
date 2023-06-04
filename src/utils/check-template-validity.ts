e100edc2d1b3f799cada3eb89e6e467610ea7d9a
Simone Sinagra <simone.sinagra@gmail.com>
2023-06-04 15:13:57 +0200

    Fix check template validity function

--- src/utils/check-template-validity.ts
+++ src/utils/check-template-validity.ts
@@ -2,6 +2,6 @@ import { templates } from '../types';
 
 import type { Template } from '../types';
 
-export function checkTemplateValidity(template: any): template is Template {
-    return typeof template === 'undefined' || templates.includes(template);
+export function checkTemplateValidity(template: unknown): template is Template {
+    return typeof template == 'string' && template in templates;
 }
